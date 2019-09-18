7ebcd9bfca3d59faaf5e882d38ac28233da23b31
Zach bylhouwer <43009020+Zombienorman0@users.noreply.github.com>
2019-09-18 13:53:11 -0600

    dwo

---
+++ Small j JavaScript/IntroLessons/SimpleVariables.js
@@ -0,0 +1,30 @@
+//console.log("did you change my file name monsieur?")
+console.log("you made it to the console! Congrats!")
+
+console.log('');// prints two spaces in console
+console.log('');// output, which is easier to read
+
+console.log("Hollo World");
+
+//create Ten Variables
+let firstName = "Zach";
+let lastName = "Bylhouwer";
+let age = 15;
+const favoriteNumber = 7;
+let hair = "Dark Brown";
+let male = true;
+let favoriteThingToDo = "Bully Athena";
+let favoriteColor = "purple";
+let favoriteGame = "Smite";
+let favoriteSentenceToHear = "Say No more Mr. Mercer";
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(favoriteNumber);
+console.log(hair);
+console.log("Am I male? " + male);
+console.log(favoriteThingToDo);
+console.log(favoriteColor);
+console.log(favoriteGame);
+console.log(favoriteSentenceToHear);
