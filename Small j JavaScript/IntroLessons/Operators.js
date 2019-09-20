1234b90ad2d3b97fbe74f699c27f5be074f32342
Zachery Bylhouwer <z.bylhouwer@EDU.epsb.ca>
2019-09-20 12:37:46 -0600

    up

---
+++ Small j JavaScript/IntroLessons/Operators.js
@@ -0,0 +1,25 @@
+//console.log("did you change my file name monsieur?")
+console.log("you made it to the console! Congrats!")
+
+function simpleOperators (numberA, numberB) {
+  let adding = numberA + numberB ;
+  let subtraction = numberA - numberB
+  let multiplying = numberA * numberB
+  let division = numberA / numberB
+  let modulus = numberA % numberB
+  console.log(numberA, numberB, 'adding', adding);
+  console.log(numberA, numberB, 'subtraction', subtraction);
+  console.log(numberA, numberB, 'multiplying', multiplying);
+  console.log(numberA, numberB, 'division', division);
+  console.log(numberA, numberB, 'modulus', modulus);
+
+}
+
+let number = [69, 149, 420, 782, 1023, 6969, 69420, 900001, '1']
+
+simpleOperators(number[0], number[1]);
+simpleOperators(number[2], number[3]);
+simpleOperators(number[4], number[5]);
+simpleOperators(number[6], number[7]);
+skip count by 6 for 10 times starting at 0;
+function skipCounting(interval);
